cea521c292e63f1d695a21c8b2e9e53b5c54cdc1
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-04-18 17:40:06 +0200

    Fine esercizio base

--- index.php
+++ index.php
@@ -42,13 +42,7 @@ $hotels = [
 
 
 
-foreach ($hotels as $hotel) {
-    echo $hotel['name'] . '<br>';
-    echo $hotel['description'] . '<br>';
-    echo $hotel['parking'] . '<br>';
-    echo $hotel['vote'] . '<br>';
-    echo $hotel['distance_to_center'] . '<br>';
-}
+
 
 
 
@@ -63,16 +57,58 @@ foreach ($hotels as $hotel) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Hotel</title>
 </head>
 
-<body>
+<body class="bg-secondary">
 
-    <form method="GET" action="index.php">
+    <h1 class="text-center mt-5 text-light">Hotels:</h1>
+
+    <table class="table m-auto mt-5 table-bordered border-light w-75">
+        <thead>
+            <tr class="text-center">
+                <th scope="col-2">Hotel</th>
+                <th scope="col-2">Description</th>
+                <th scope="col-2">Parking</th>
+                <th scope="col-2">Vote</th>
+                <th scope="col-2">Distance To Center</th>
+            </tr>
+        </thead>
+        <tbody class="text-center">
+            <?php foreach ($hotels as $hotel) { ?>
+                <tr>
+                    <th scope="row">
+                        <?php echo $hotel['name'] ?>
+                    </th>
+                    <td>
+                        <?php echo $hotel['description'] ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['parking'] ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['vote'] ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['distance_to_center'] ?>
+                    </td>
+
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 
 
-    </form>
 
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
